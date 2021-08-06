b4dd43216ec77be8918391ec291b170f81693c33
Naofumi Yamada <17900178+na0fu3y@users.noreply.github.com>
2021-08-06 15:43:04 +0900

    blackとisortパッケージを追加

--- test_main.py
+++ test_main.py
@@ -1,12 +1,15 @@
-from main import validate_role, validate_exclude_members
 import os
 
+from main import validate_exclude_members, validate_role
+
+
 def test_validate_role():
-    os.environ['CONFIG_YAML_PATH'] = 'config.yml'
-    assert validate_role('hoge') == False
-    assert validate_role('roles/cloudscheduler.jobRunner') == True
+    os.environ["CONFIG_YAML_PATH"] = "config.yml"
+    assert validate_role("hoge") == False
+    assert validate_role("roles/cloudscheduler.jobRunner") == True
+
 
 def test_validate_exclude_members():
-    os.environ['CONFIG_YAML_PATH'] = 'config.yml'
-    assert validate_exclude_members('hoge') == False
-    assert validate_exclude_members('test.user1@example.com') == True
+    os.environ["CONFIG_YAML_PATH"] = "config.yml"
+    assert validate_exclude_members("hoge") == False
+    assert validate_exclude_members("test.user1@example.com") == True
